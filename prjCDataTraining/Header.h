e3e22dcfb08e1deafd5efe8e2e336337893a730a
kaustah <kaustah.henriques@gmail.com>
2020-12-25 22:38:08 -0500

    2020-12-24 Review TODOs

--- prjCDataTraining/Header.h
+++ prjCDataTraining/Header.h
@@ -11,11 +11,11 @@ typedef union {
 
 } interval_duree;
 
-typedef struct { //TODO: A voir pour merge avec struct: exercice_sheet
+typedef struct {
 	char nom_exercice[100];
 	short nb_set;
 	short temps_recup;
-	interval_duree interval; //TODO:A voir pour le nom de la variable
+	interval_duree interval;
 	short type_interval;
 	float weight;
 
@@ -26,12 +26,13 @@ typedef struct {
 	short nb_set_accompli;
 	interval_duree interval[5]; //TODO: Voir Malloc pour reduire l'utilisation de memoire + ligne du dessous Code5
 	float weight[5];
+	exercice_template *template;
 
 } exercice_sheet;
 
 typedef struct {
 	struct tm *tm; //TODO: A voir et comprendre comment manipuler le temps en C. Adapter en consequence//Date
-	time_t starttime;
+	time_t starttime;//TODO: Creer fonction afin de retourner le format Date Heure en String pour insertion SQL
 	time_t endtime;
 	exercice_template type_exercice[10];	//TODO: Code5
 	exercice_sheet sheet_exercice[10];		//TODO: Code5
